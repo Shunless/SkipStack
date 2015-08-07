86b8587fbf19d9331ca74c9589a0c26e0df10dca
Mourtz <alex.mourtziapis@gmail.com>
2015-08-07 10:39:37 +0300

    Updated build system.

--- build/build.sh
+++ build/build.sh
@@ -34,8 +34,4 @@ fi
 sudo npm install uglify-js -g
 sudo apt-get -qq update
 
-uglifyjs ../src/Assets/**/*.js \
-         ../src/**/*.js \
-         -o skipstack.min.js \
-         --source-map skipstack.min.js.map \
-         -p 5 -c -m
+uglifyjs ../src/Assets/Shaders.js ../src/Assets/Tunes.js ../src/Actor.js ../src/Cell.js ../src/Color.js ../src/Enemy.js ../src/Grid.js ../src/index.js ../src/Math.js ../src/Swipe.js ../src/uiBuilder.js ../src/uiDivider.js  ../src/uiGeneric.js ../src/uiIndex.js ../src/uiSelector.js -o skipstack.min.js --source-map skipstack.min.js.map -c -m
